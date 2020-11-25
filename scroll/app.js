4cbd0c8f7624725a208edd53a3d2ac84c3ca84f8
jeongmyeong <kimbym23@gmail.com>
2020-11-25 13:10:58 +0900

    animation with images 초안

--- scroll/app.js
+++ scroll/app.js
@@ -1,5 +1,5 @@
 import * as ScrollMagic from "scrollmagic"; // Or use scrollmagic-with-ssr to avoid server rendering problems
-import { TweenMax, TimelineMax } from "gsap"; // Also works with TweenLite and TimelineLite
+import { gsap, TweenMax, TimelineMax } from "gsap"; // Also works with TweenLite and TimelineLite
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
 
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
@@ -20,7 +20,7 @@ let scene = new ScrollMagic.Scene({
 const text = intro.querySelector('h1');
 const textAnim = TweenMax.fromTo(text, 1, {opacity: 0}, {opacity: 1});
 
-let scene2 = new ScrollMagic.Scene({
+new ScrollMagic.Scene({
     duration: 3000,
     triggerElement: intro,
     triggerHook: 0
@@ -41,3 +41,47 @@ setInterval(() => {
 
     video.currentTime = delay;
 }, 33);
+
+
+const second = document.querySelector('.second');
+const ctx = second.getContext('2d');
+
+let scene3 = new ScrollMagic.Scene({
+    duration: 3500,
+    triggerElement: second,
+    triggerHook: 0
+})
+    .setPin(intro)
+    .addTo(controller);
+
+const frameCount = 147;
+const currentFrame = index => (
+    `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${(index + 1).toString().padStart(4, '0')}.jpg`
+);
+
+const images = []
+const airpods = {
+    frame: 0
+};
+
+for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+    images.push(img);
+}
+
+gsap.to(airpods, {
+    frame: frameCount - 1,
+    snap: "frame",
+    scrollTrigger: {
+        scrub: 0.5
+    },
+    onUpdate: render
+});
+
+images[0].onload = render;
+
+function render() {
+    ctx.clearRect(0, 0, document.body.clientWidth, document.body.clientHeight);
+    ctx.drawImage(images[airpods.frame], 0, 0);
+}
